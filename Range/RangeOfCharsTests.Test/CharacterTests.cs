e077664a2c3f2005b7269d37fdbc30828323aec1
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-02 17:59:25 +0300

    another 4 Tests for Character class

--- Range/RangeOfCharsTests.Test/CharacterTests.cs
+++ Range/RangeOfCharsTests.Test/CharacterTests.cs
@@ -25,39 +25,40 @@ namespace RangeOfChars.Test
             Assert.False(actual);
         }
 
-        //[Fact]
-        //public void For_1ABC_Should_Return_False()
-        //{
-        //    var digits = new Range('a', 'f');
-        //    string text = "1abc";
-        //    bool actual = digits.Match(text);
-        //    Assert.False(actual);
-        //}
+        [Fact]
+        public void For_1ABC_Should_Return_False()
+        {
+            var character = new Character('a');
+            string text = "1abc";
+            bool actual = character.Match(text);
+            Assert.False(actual);
+        }
 
-        //[Fact]
-        //public void For_Null_String_Should_Return_False()
-        //{
-        //    var digits = new Range('a', 'f');
-        //    string text = "";
-        //    bool actual = digits.Match(text);
-        //    Assert.False(actual);
-        //}
+        [Fact]
+        public void For_Null_String_Should_Return_False()
+        {
+            var character = new Character('a');
+            string text = "";
+            bool actual = character.Match(text);
+            Assert.False(actual);
+        }
 
-        //[Fact]
-        //public void For_Empty_Should_Return_False()
-        //{
-        //    var digits = new Range('a', 'f');
-        //    string text = " ";
-        //    bool actual = digits.Match(text);
-        //    Assert.False(actual);
-        //}
-        //[Fact]
-        //public void For_A_Space_In_String_Should_Return_False()
-        //{
-        //    var digits = new Range('a', 'f');
-        //    string text = " abc ";
-        //    bool actual = digits.Match(text);
-        //    Assert.False(actual);
-        //}
+        [Fact]
+        public void For_Empty_Should_Return_False()
+        {
+            var character = new Character('f');
+            string text = " ";
+            bool actual = character.Match(text);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void For_A_Space_In_String_Should_Return_False()
+        {
+            var character = new Character('a');
+            string text = " abc ";
+            bool actual = character.Match(text);
+            Assert.False(actual);
+        }
     }
 }
