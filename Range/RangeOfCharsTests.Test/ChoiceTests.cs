0bd3496a6757625db0dedb10bba338cadbb0ceae
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-03 11:50:04 +0300

    2 more Tests added to Choice class

--- Range/RangeOfCharsTests.Test/ChoiceTests.cs
+++ Range/RangeOfCharsTests.Test/ChoiceTests.cs
@@ -18,5 +18,29 @@ namespace RangeOfChars.Test
             bool actual = digit.Match("024");
             Assert.True(actual);
         }
+
+        [Fact]
+        public void Should_Return_True_For_Range_1_To_9()
+        {
+            var digit = new Choice
+            (
+                new Character('0'),
+                new Range('1', '9')
+            );
+            bool actual = digit.Match("214");
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Should_Return_False_For_1024()
+        {
+            var digit = new Choice
+            (
+                new Character('0'),
+                new Range('2', '9')
+            );
+            bool actual = digit.Match("1024");
+            Assert.False(actual);
+        }
     }
 }
