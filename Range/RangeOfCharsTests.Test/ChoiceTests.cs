34807014de8cc6b5faa03a83b2b3a75a74133a5d
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-03 12:00:36 +0300

    3 more Tests added plus the first 2 modified

--- Range/RangeOfCharsTests.Test/ChoiceTests.cs
+++ Range/RangeOfCharsTests.Test/ChoiceTests.cs
@@ -27,19 +27,55 @@ namespace RangeOfChars.Test
                 new Character('0'),
                 new Range('1', '9')
             );
-            bool actual = digit.Match("214");
+            bool actual = digit.Match("12");
             Assert.True(actual);
         }
 
         [Fact]
-        public void Should_Return_False_For_1024()
+        public void Should_Return_False_For_92()
         {
             var digit = new Choice
             (
                 new Character('0'),
                 new Range('2', '9')
             );
-            bool actual = digit.Match("1024");
+            bool actual = digit.Match("92");
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Should_Return_False_For_a9()
+        {
+            var digit = new Choice
+            (
+                new Character('0'),
+                new Range('2', '9')
+            );
+            bool actual = digit.Match("a9");
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Should_Return_False_For_Empty_String()
+        {
+            var digit = new Choice
+            (
+                new Character('0'),
+                new Range('2', '9')
+            );
+            bool actual = digit.Match("");
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Should_Return_False_For_Null()
+        {
+            var digit = new Choice
+            (
+                new Character('0'),
+                new Range('2', '9')
+            );
+            bool actual = digit.Match(null);
             Assert.False(actual);
         }
     }
