38e7840052988258834f8deab125bf3fa50043a9
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-13 18:50:10 +0300

    3 more Tests added to AnyTests

--- Range/RangeOfCharsTests.Test/AnyTests.cs
+++ Range/RangeOfCharsTests.Test/AnyTests.cs
@@ -40,11 +40,35 @@ namespace RangeOfChars.Test
         }
 
         [Fact]
+        public void For_String_a_Should_Return_False()
+        {
+            var e = new Any("eE");
+            IMatch actual = e.Match("a");
+            Assert.False(actual.Succes());
+        }
+
+        [Fact]
+        public void For_String_a_Should_Return_a()
+        {
+            var e = new Any("eE");
+            IMatch actual = e.Match("a");
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
         public void For_Empty_String_Should_Return_Empty_String()
         {
             var e = new Any("eE");
             IMatch actual = e.Match(string.Empty);
             Assert.Equal(string.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_Empty_String_Should_Return_False()
+        {
+            var e = new Any("eE");
+            IMatch actual = e.Match(string.Empty);
+            Assert.False(actual.Succes());
         }
     }
 }
