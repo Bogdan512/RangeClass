b6e4b34e12dca8c0cefb68b8f4c612c9c4e27b5a
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-16 22:37:34 +0300

    2 more tests added to class OptionalTests with Character_Minus_and_String_123

--- Range/RangeOfCharsTests.Test/OptionalTests.cs
+++ Range/RangeOfCharsTests.Test/OptionalTests.cs
@@ -70,5 +70,37 @@ namespace RangeOfChars.Test
             IMatch actual = a.Match(string.Empty);
             Assert.Equal(string.Empty, actual.RemainingText());
         }
+
+        [Fact]
+        public void For_Character_a_and_String_null_Should_Return_True()
+        {
+            var a = new Optional(new Character('a'));
+            IMatch actual = a.Match(null);
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void For_Character_a_and_String_null_Should_Return_null()
+        {
+            var a = new Optional(new Character('a'));
+            IMatch actual = a.Match(null);
+            Assert.Equal(null, actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_Character_Minus_and_String_123_Should_Return_true()
+        {
+            var sign = new Optional(new Character('-'));
+            IMatch actual = sign.Match("123");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void For_Character_Minus_and_String_123_Should_Return_123()
+        {
+            var a = new Optional(new Character('a'));
+            IMatch actual = a.Match("123");
+            Assert.Equal("123", actual.RemainingText());
+        }
     }
 }
