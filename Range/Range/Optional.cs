e279a3d6f77e6b48c50874c3776918960286b866
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-16 22:19:02 +0300

    Implemented class Optional and added 2 tests to class OptionalTests

---
+++ Range/Range/Optional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeOfChars
+{
+    public class Optional
+    {
+        IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            IMatch match = new Match(true, text);
+            match = this.pattern.Match(match.RemainingText());
+
+            return new Match(true, match.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeOfCharsTests.Test/OptionalTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeOfChars.Test
+{
+    public class OptionalTests
+    {
+        [Fact]
+        public void For_Character_a_and_String_abc_Should_Return_True()
+        {
+            var a = new Optional(new Character('a'));
+            IMatch actual = a.Match("abc");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void For_Character_a_and_String_abc_Should_Return_bc()
+        {
+            var a = new Optional(new Character('a'));
+            IMatch actual = a.Match("abc");
+            Assert.Equal("bc", actual.RemainingText());
+        }
+    }
+}
