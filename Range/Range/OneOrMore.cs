0a36046810df4d15348184d6f0ccf34b2c57a9b1
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-18 18:35:56 +0300

    class OnOrMore without any comments..clean

--- Range/Range/OneOrMore.cs
+++ Range/Range/OneOrMore.cs
@@ -13,32 +13,12 @@ namespace RangeOfChars
              this.many = new Many(pattern);
         }
 
-
         public IMatch Match(string text)
         {
-            IMatch match = many.Match(text);
-
+            IMatch match = this.many.Match(text);
             return text == match.RemainingText()
                 ? new Match(false, match.RemainingText())
                 : new Match(true, match.RemainingText());
         }
-
-
-        //public IMatch Match(string text)
-        //{
-        //    IMatch match = new Match(true, text);
-        //    match = this.pattern.Match(match.RemainingText());
-        //    if (!match.Succes())
-        //    {
-        //        return match;
-        //    }
-
-        //    while (match.Succes())
-        //    {
-        //        match = this.pattern.Match(match.RemainingText());
-        //    }
-
-        //    return new Match(true, match.RemainingText());
-        //}
     }
 }
