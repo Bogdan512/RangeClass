97d3fd37aa2b03cacf36ffc333484c034672f751
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-21 19:58:11 +0300

    List class implemented and 2 tests for ListTests

---
+++ Range/Range/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeOfChars
+{
+    public class List : IPattern
+    {
+        IPattern pattern;
+        IPattern separator;
+
+        public List(IPattern pattern, IPattern separator)
+        {
+            this.pattern = pattern;
+            this.separator = separator;
+        }
+
+        public IMatch Match(string text)
+        {
+
+            IMatch match = pattern.Match(text);
+            match = separator.Match(match.RemainingText());
+            return match;
+        }
+    }
+}
---
+++ Range/RangeOfCharsTests.Test/ListTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeOfChars.Test
+{
+    public class ListTests
+    {
+        [Fact]
+        public void It_Should_Consume_A_Pattern_And_A_Character_Returning_True()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1,");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void It_Should_Consume_A_Pattern_And_A_Character_With_RemainingText_Empty()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1,");
+            Assert.Equal("", actual.RemainingText());
+        }
+    }
+}
