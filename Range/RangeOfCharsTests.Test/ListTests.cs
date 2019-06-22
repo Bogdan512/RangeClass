087af798ce0b95557a21f14573a6a16d5878e186
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-22 08:35:32 +0300

    added 4 tests to ListTests

--- Range/RangeOfCharsTests.Test/ListTests.cs
+++ Range/RangeOfCharsTests.Test/ListTests.cs
@@ -22,5 +22,53 @@ namespace RangeOfChars.Test
             IMatch actual = a.Match("1,");
             Assert.Equal(",", actual.RemainingText());
         }
+
+        [Fact]
+        public void It_Should_Consume_All_The_Patterns_And_Characters_Returning_True()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1,2,3");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void It_Should_Consume_All_The_Patterns_And_Characters_With_RemainingText_Empty()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1,2,3");
+            Assert.Equal(string.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void It_Should_Consume_All_The_Patterns_And_Characters_Except_Last_Coma_Returning_True()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1,2,3,");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void It_Should_Consume_All_The_Patterns_And_Characters_Returning_A_Coma()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1,2,3,");
+            Assert.Equal(",", actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_String_1a_It_Should_Consume_Only_The_1_And_Return_True()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1a");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void For_String_1a_It_Should_Consume_Only_The_1_And_Return_a()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match("1a");
+            Assert.Equal("a", actual.RemainingText());
+        }
     }
 }
