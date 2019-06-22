f8a3b545dd3e872dab1174a6c057599db8795e54
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-22 09:01:25 +0300

    3 more complex tests added  to ListTests

--- Range/RangeOfCharsTests.Test/ListTests.cs
+++ Range/RangeOfCharsTests.Test/ListTests.cs
@@ -70,5 +70,49 @@ namespace RangeOfChars.Test
             IMatch actual = a.Match("1a");
             Assert.Equal("a", actual.RemainingText());
         }
+
+        [Fact]
+        public void For_Empty_String_It_Should_Return_Empty_String()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            IMatch actual = a.Match(string.Empty);
+            Assert.Equal(string.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_Digits_Separators_NewLine_And_Tab_Should_Return_Empty_String()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequance(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            IMatch actual = list.Match("1; 22  ;\n 333 \t; 22");
+            Assert.Equal(string.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_Number_And_NewLine_Should_Return_NewLine()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequance(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            IMatch actual = list.Match("1 \n");
+            Assert.Equal(" \n", actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_String_abc_Should_Return_abc()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequance(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            IMatch actual = list.Match("abc");
+            Assert.Equal("abc", actual.RemainingText());
+        }
     }
 }
