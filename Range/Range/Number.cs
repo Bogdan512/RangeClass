62be9c388f7d9b638eee3310b0d8e37a37c03c13
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-24 21:51:27 +0300

    implemented classes Number , and ValidateNumber  and added 2 tests to NumberTests

---
+++ Range/Range/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RangeOfChars
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return this.pattern.Match(text);
+        }
+    }
+}
---
+++ Range/RangeOfCharsTests.Test/NumberTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace RangeOfChars
+{
+    public class NumberTests
+    {
+        [Fact]
+        public void For_234_It_Should_Return_True()
+        {
+            var number = new Number(new ValidateNumber());
+            IMatch actual = number.Match("234");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void For_0234_It_Should_Return_False()
+        {
+            var number = new Number(new ValidateNumber());
+            IMatch actual = number.Match("0234");
+            Assert.False(actual.Succes());
+        }
+    }
+}
