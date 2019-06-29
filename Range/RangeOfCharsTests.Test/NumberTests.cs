6bae3a55aed505d4e66fe42f5ba34fa60cccfe03
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-06-29 11:27:17 +0300

    class Number refactored for leading zeros and 4 more tests added to NumberTests

--- Range/RangeOfCharsTests.Test/NumberTests.cs
+++ Range/RangeOfCharsTests.Test/NumberTests.cs
@@ -22,19 +22,51 @@ namespace RangeOfChars.Test
         }
 
         [Fact]
-        public void For_02_It_Should_Return_False()
+        public void For_0002_It_Should_Return_False()
         {
-            var number = new Number(new Sequance(new Range('1', '9'), new Range('0', '9')));
-            IMatch actual = number.Match("02");
+            var number = new Number(
+                new Sequance(
+                    new Range('1', '9'),
+                    new Many(new Range('0', '9'))));
+
+            IMatch actual = number.Match("0002");
             Assert.False(actual.Succes());
         }
 
         [Fact]
         public void For_02_It_Should_Return_02()
         {
-            var number = new Number(new Sequance(new Range('1', '9'), new Range('0', '9')));
+            var number = new Number(
+                new Sequance(
+                    new Range('1', '9'),
+                    new Many(new Range('0', '9'))));
+
             IMatch actual = number.Match("02");
             Assert.Equal("02", actual.RemainingText());
+        }
+
+        [Fact]
+        public void For_20708_It_Should_Return_True()
+        {
+            var number = new Number(
+                new Sequance(
+                    new Range('1', '9'),
+                    new Many(new Range('0', '9'))));
+
+            IMatch actual = number.Match("20708");
+            Assert.True(actual.Succes());
+        }
+
+        [Fact]
+        public void For_20708_It_Should_Return_EmptyString()
+        {
+            var number = new Number(
+                new Sequance(
+                    new Range('1', '9'),
+                    new Many(new Range('0', '9'))));
+
+            IMatch actual = number.Match("207");
+            Assert.Equal(string.Empty, actual.RemainingText());
         }
     }
 }
