bf18ad7ae566a4141389222350bd14b1e87997f2
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-07-04 20:49:46 +0300

    refactored class Number for integers and floats, clean , and tests

--- Range/Range/Number.cs
+++ Range/Range/Number.cs
@@ -5,9 +5,6 @@ namespace RangeOfChars
     public class Number : IPattern
     {
         private readonly IPattern pattern;
-        private readonly IPattern patternNoZeroFloat;
-        //private readonly IPattern patternSignZeroFloats;
-        //private readonly IPattern patternFloats;
 
         public Number()
         {
@@ -18,7 +15,6 @@ namespace RangeOfChars
             var digits = new Many(digit);
             var natural = new Choice(zeroChar, new Sequance(new Range('1', '9'), digits));
             var integer = new Sequance(minus, natural);
-            var zeroFloat = new Sequance(minus, zeroChar, dot, digits);
             pattern = new Sequance(integer, new Optional(new Sequance(dot, digits)));
 
 
