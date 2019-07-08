35132d4f8ba0dce81e9999e178e13aa44ad002bf
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-07-08 20:47:04 +0300

    clean class Number and name bigOrSmall changed to e

--- Range/Range/Number.cs
+++ Range/Range/Number.cs
@@ -14,10 +14,10 @@ namespace RangeOfChars
             var digit = new Range('0', '9');
             var digits = new Many(digit);
             var sign = new Any("+-");
-            var bigOrSmall = new Any("Ee");
+            var e = new Any("Ee");
             var natural = new Choice(zeroChar, new Sequance(new Range('1', '9'), digits));
             var integer = new Sequance(minus, natural);
-            this.pattern = new Sequance(integer, new Optional(new Sequance(dot, digits, new Optional(new Sequance(bigOrSmall, new Optional(sign), digits)))));
+            this.pattern = new Sequance(integer, new Optional(new Sequance(dot, digits, new Optional(new Sequance(e, new Optional(sign), digits)))));
         }
 
         public IMatch Match(string text)
