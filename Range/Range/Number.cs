de8ad1cae45e711a1a63d50406f8a869eff5a7d3
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-07-08 20:45:36 +0300

    clean class Number finished

--- Range/Range/Number.cs
+++ Range/Range/Number.cs
@@ -9,7 +9,6 @@ namespace RangeOfChars
         public Number()
         {
             var minus = new Optional(new Character('-'));
-            var plus = new Optional(new Character('+'));
             var dot = new Character('.');
             var zeroChar = new Character('0');
             var digit = new Range('0', '9');
@@ -18,8 +17,7 @@ namespace RangeOfChars
             var bigOrSmall = new Any("Ee");
             var natural = new Choice(zeroChar, new Sequance(new Range('1', '9'), digits));
             var integer = new Sequance(minus, natural);
-            //pattern = new Sequance(integer, new Optional(new Sequance(dot, digits, new Optional(bigOrSmall), new Optional(sign), digits)));
-            pattern = new Sequance(integer, new Optional(new Sequance(dot, digits, new Optional(new Sequance(bigOrSmall, new Optional(sign), digits)))));
+            this.pattern = new Sequance(integer, new Optional(new Sequance(dot, digits, new Optional(new Sequance(bigOrSmall, new Optional(sign), digits)))));
         }
 
         public IMatch Match(string text)
